0744ad28f2cda1e3512b96a851c1c9153f61dc3f
姜沅初 <jiang_yc@suixingpay.com>
2020-03-10 11:55:23 +0800

    毕业设计

---
+++ src/main/java/com/jyc/graduation/GraduationApplication.java
@@ -0,0 +1,11 @@
+package com.jyc.graduation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GraduationApplication.class);
+    }
+}
